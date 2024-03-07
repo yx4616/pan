a7edab5106b3f41d00d93fd7b0eb70a06b43e75a
yx4616 <yx4616@outlook.com>
2024-03-07 22:13:46 +0800

    redis 添加

---
+++ src/main/java/com/pan/component/RedisComponent.java
@@ -0,0 +1,24 @@
+package com.pan.component;
+
+import com.pan.domain.constants.Constants;
+import com.pan.domain.dto.UserSpaceDto;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component("redisComponent")
+public class RedisComponent {
+
+    @Resource
+    private RedisUtils redisUtils;
+
+    /**
+     * 保存已使用的空间
+     *
+     * @param userId
+     */
+    public void saveUserSpaceUse(String userId, UserSpaceDto userSpaceDto) {
+        redisUtils.setex(Constants.REDIS_KEY_USER_SPACE_USE + userId, userSpaceDto, Constants.REDIS_KEY_EXPIRES_DAY);
+    }
+
+}
